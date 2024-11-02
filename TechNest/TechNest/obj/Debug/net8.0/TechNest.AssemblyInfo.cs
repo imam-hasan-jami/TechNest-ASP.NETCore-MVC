3c9e790c4e8b3f2205039e146f34d9107d0655c3
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-02 20:42:34 +0600

    updated .gitignore

--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace0b2dc7b6596eb9196213b85565a596535b50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0585e9841f9a1a5770ec9507d5fd6ea7af2de599")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
