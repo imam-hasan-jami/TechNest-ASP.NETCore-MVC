57115f382e982adad25a0bce1ad590901506046f
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-04 22:21:09 +0600

    View Products

---
+++ TechNest/TechNest/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechNest.Services;
+
+namespace TechNest.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+    }
+}
