c645172c5dab97b5c20836eb0adae073c32c346b
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-17 19:08:35 +0600

    Added Clear Button
    Added clear button in the Store (Index)

--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67dfe881e96c1c8d64f5116e93a84af564db0e46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78107fd046bf1f7b8407159e53d35ee37c1c434c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
