96c673c86f3dad7219648c9735583d359baca733
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-22 20:20:03 +0600

    Created RegisterDTO model for User Registration

---
+++ TechNest/TechNest/Models/RegisterDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechNest.Models
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "The First Name field is required"), MaxLength(100)]
+        public string FirstName { get; set; } = "";
+        [Required(ErrorMessage = "The Last Name field is required"), MaxLength(100)]
+        public string LastName { get; set; } = "";
+        [Required(ErrorMessage = "The Email field is required"), EmailAddress, MaxLength(100)]
+        public string Email { get; set; } = "";
+        [Phone(ErrorMessage = "The format of the phone number is invalid"), MaxLength(20)]
+        public string? PhoneNumber { get; set; }
+        [Required, MaxLength(200)]
+        public string Address { get; set; } = "";
+        [Required, MaxLength(100)]
+        public string Password { get; set; } = "";
+        [Required(ErrorMessage = "The Confirm Password field is required")]
+        [Compare("Password", ErrorMessage = "The password and confirm password do not match")]
+        public string ConfirmPassword { get; set; } = "";
+    }
+}
--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b15b7e3319ad04da1217b05c50f05dc8e88cae7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38dac6d5b80d46200cda507644407fb0219f415b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
