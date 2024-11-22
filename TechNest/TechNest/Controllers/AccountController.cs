6e740cbf9de49c29312eb8df37f7f46b9f493c8d
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-23 00:58:54 +0600

    Create User Account

--- TechNest/TechNest/Controllers/AccountController.cs
+++ TechNest/TechNest/Controllers/AccountController.cs
@@ -19,5 +19,47 @@ namespace TechNest.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(registerDTO);
+            }
+
+            //create a new account and authenticate the user
+            var user = new ApplicationUser()
+            {
+                FirstName = registerDTO.FirstName,
+                LastName = registerDTO.LastName,
+                UserName = registerDTO.Email,       //username will be used to authenticate the user
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.PhoneNumber,
+                Address = registerDTO.Address,
+                CreatedAt = DateTime.Now
+            };
+
+            var result = await userManager.CreateAsync(user, registerDTO.Password);
+
+            if (result.Succeeded)
+            {
+                //successfull user registration
+                await userManager.AddToRoleAsync(user, "client");
+
+                //sign in the new user
+                await signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            // registration failed => show registration errors
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(registerDTO);
+        }
     }
 }
--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96c673c86f3dad7219648c9735583d359baca733")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063f920f52cd463a0102a843c568531a1ce9d336")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
