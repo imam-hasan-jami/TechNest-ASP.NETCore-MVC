b23761282ac9eac4ae452983b3c7ae500c28bb11
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-23 13:48:06 +0600

    Updated the Navbar
    login and registration button won't be visible if the user is already logged in

--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f840afc7542a28338b0ae31e9f2195dd4dde426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756a3118fed1fb8f6f023cfe844459d63c0ca252")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
