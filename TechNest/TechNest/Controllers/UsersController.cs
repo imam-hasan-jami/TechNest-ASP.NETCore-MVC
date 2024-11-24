1a202e6acfee82c16b4faa6b18e70947a088c44c
IMAM HASAN JAMI <55746728+imam-hasan-jami@users.noreply.github.com>
2024-11-24 17:51:17 +0600

    Show Registered Users
    Admin can show the registered users

---
+++ TechNest/TechNest/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TechNest.Models;
+
+namespace TechNest.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/Admin/[controller]/{action=Index}/{id?}")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var users = userManager.Users.OrderByDescending(u => u.CreatedAt).ToList();
+
+            return View(users);
+        }
+    }
+}
--- TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
+++ TechNest/TechNest/obj/Debug/net8.0/TechNest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c905bb77a9d31cd0c2a7610aa8c5323ef73cb9d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aea3aa3217a2d5c87a79959464d45616a603fb53")]
 [assembly: System.Reflection.AssemblyProductAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TechNest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
